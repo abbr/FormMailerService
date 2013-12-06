0f85f3d2e9edc194e943968c5c9680d238ae4f2a
Fred <wenfred@gmail.com>
2013-12-06 14:40:07 -0800

    included angular-ui-bootstrap in order to use modal window etc.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,16 @@
 'use strict';
 
-angular.module('formMailerServiceApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .when('/admin', {
-        templateUrl: 'partials/admin',
-        controller: 'AdminCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
+angular.module('formMailerServiceApp',
+    [ 'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute','ui.bootstrap']).config(
+    function($routeProvider, $locationProvider) {
+      $routeProvider.when('/', {
+        templateUrl : 'partials/main',
+        controller : 'MainCtrl'
+      }).when('/admin', {
+        templateUrl : 'partials/admin',
+        controller : 'AdminCtrl'
+      }).otherwise({
+        redirectTo : '/'
       });
-    $locationProvider.html5Mode(true);
-  });
+      $locationProvider.html5Mode(true);
+    });
