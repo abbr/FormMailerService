fff09657384e87539ce9cb7ffbae2338bf1133fc
Fred <wenfred@gmail.com>
2013-12-07 12:39:27 -0800

    use put method to update item.

--- app/scripts/services/sites.js
+++ app/scripts/services/sites.js
@@ -1,5 +1,7 @@
 'use strict';
 
 angular.module('formMailerServiceApp').factory('Sites', [ '$resource', function($resource) {
-	return $resource('api/sites/:id');
+	return $resource('api/sites/:id',{},{
+	  save: {method:'PUT'}
+	});
 } ]);
--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var api = require('./lib/controllers/api'),
 // Server Routes
 app.get('/api/sites*', api.getSites);
 app.delete('/api/sites/*', api.deleteSite);
-app.post('/api/sites/*', api.updateSite);
+app.put('/api/sites/*', api.updateSite);
 app.post('/api/sites/?', api.createSite);
 
 // Angular Routes
