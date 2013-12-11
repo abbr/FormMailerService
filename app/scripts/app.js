50de370c7d03cfcd74959ebe2aef4baf4b26e3fb
Fred <wenfred@gmail.com>
2013-12-10 21:33:06 -0800

    used yo angular-fullstack:route to generate users

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
-angular.module('formMailerServiceApp',
-    [ 'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute','ui.bootstrap']).config(
-    function($routeProvider, $locationProvider) {
-      $routeProvider.when('/', {
-        templateUrl : 'partials/main',
-        controller : 'MainCtrl'
-      }).when('/admin', {
-        templateUrl : 'partials/admin',
-        controller : 'AdminCtrl'
-      })
-      .when('/admin/help', {
-        templateUrl: 'partials/help'
-      }).otherwise({
-        redirectTo : '/'
-      });
-      $locationProvider.html5Mode(true);
-    });
+angular.module('formMailerServiceApp', [ 'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'ui.bootstrap' ]).config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl : 'partials/main',
+    controller : 'MainCtrl'
+  }).when('/admin', {
+    templateUrl : 'partials/admin',
+    controller : 'AdminCtrl'
+  }).when('/admin/help', {
+    templateUrl : 'partials/help'
+  }).when('/admin/users', {
+    templateUrl : 'partials/users',
+    controller : 'UsersCtrl'
+  }).otherwise({
+    redirectTo : '/'
+  });
+  $locationProvider.html5Mode(true);
+});
