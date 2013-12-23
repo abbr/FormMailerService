d4cfa37595edff33f2d5483d67f7c03adc145452
abbr <wenfred@gmail.com>
2013-12-22 20:15:31 -0800

    removed most of the test scripts

--- test/spec/services/users.js
+++ test/spec/services/users.js
@@ -1,18 +1,12 @@
 'use strict';
 
-describe('Service: Users', function () {
+describe('Service: Users', function() {
 
   // load the service's module
   beforeEach(module('formMailerServiceApp'));
 
-  // instantiate service
-  var Users;
-  beforeEach(inject(function (_Users_) {
-    Users = _Users_;
-  }));
-
-  it('should do something', function () {
-    expect(!!Users).toBe(true);
+  it('should do something', function() {
+    expect(true).toBe(true);
   });
 
 });
