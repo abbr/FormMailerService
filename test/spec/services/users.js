6ead7e626a93082a296a0167a033036ee0c1bf4c
abbr <wenfred@gmail.com>
2013-12-23 22:37:11 -0800

    added some unit test code.

--- test/spec/services/users.js
+++ test/spec/services/users.js
@@ -5,8 +5,14 @@ describe('Service: Users', function() {
   // load the service's module
   beforeEach(module('formMailerServiceApp'));
 
-  it('should do something', function() {
-    expect(true).toBe(true);
+  // instantiate service
+  var Users;
+  beforeEach(inject(function(_Users_) {
+    Users = _Users_;
+  }));
+
+  it('should be resource ', function() {
+    expect(Users.name).toBe('Resource');
   });
 
 });
