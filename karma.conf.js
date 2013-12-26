dafe8924ca878d1cd96a74b31904a8e179ce32f2
abbr <wenfred@gmail.com>
2013-12-25 17:00:23 -0800

    make test working

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'app/bower_components/angular-touch/angular-touch.js',
       'app/bower_components/angular-ui-utils/modules/validate/validate.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
